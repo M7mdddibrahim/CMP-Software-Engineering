50acf323784d98854b78e980750523234e9e6f3b
Youssef-Awad2004 <Youssef.Hassan04@eng-st.cu.edu.eg>
2025-02-15 12:19:25 +0200

    added logic to employee.js

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -8,8 +8,23 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
+  const {id}=req.params.id;
+  const index=employee.filter((emp)=>emp.id!==id);
+  if (index !=-1)
+  {
+    employee.splice(index,1);
+    res.status(200).json({data:employee});
+  }
+  else
+  {
+    res.status(404).json({message:"Employee not found"});
+  }
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+  const {id,name}=req.body;
+  const newEmployee={id,name};
+  employee.push(newEmployee);
+  res.status(201).json({data:employee});
 };
